83f1d082a11dc9062eb41c89c8aac616ba8c9849
Bram Hoendervangers <bram.hoendervangers@gmail.com>
2022-08-13 12:48:54 +0200

    feat: vite library scaffold

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), dts({ insertTypesEntry: true })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/index.ts"),
+      name: "MyLib",
+      formats: ["es", "umd"],
+      // the proper extensions will be added
+      fileName: "my-lib",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react", "react-dom"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
