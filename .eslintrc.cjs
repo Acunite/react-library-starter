573868f9fc61e11f8e9fff6be5b1e3e7a609e620
Bram Hoendervangers <bram.hoendervangers@gmail.com>
2022-08-13 13:25:37 +0200

    feat: prettier eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
